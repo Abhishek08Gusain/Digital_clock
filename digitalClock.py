0fc2056727d571ce73a57a79ed4c689705128e4b
Abhishek <abhishekgusain0803@gmail.com>
2023-03-22 16:46:17 +0800

    removed welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -5,7 +5,7 @@ window.title("Digital CLock")
 window.geometry("600x300")
 window.configure(bg="blue")
 
-Label = Label(window, text="Welcome!", font=("Arial Black",78,"bold"), bg="blue", fg="white")
+Label = Label(window, font=("Arial Black",78,"bold"), bg="blue", fg="white")
 Label.pack(pady=100)
 
 window.mainloop()
