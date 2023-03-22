eff529539e59cfcbd649aed731907a15b6b7e718
Abhishek <abhishekgusain0803@gmail.com>
2023-03-22 18:22:18 +0800

    added digital clock to the screen

--- digitalClock.py
+++ digitalClock.py
@@ -1,11 +1,21 @@
-from tkinter import Tk, Label
+import tkinter as tk
+from time import strftime
 
-window = Tk()
-window.title("Digital CLock")
-window.geometry("600x300")
-window.configure(bg="blue")
+def update_time():
+    time_str = strftime('%H:%M:%S %p')
+    clock_label.config(text=time_str)
+    clock_label.after(1000, update_time)
 
-Label = Label(window, font=("Arial Black",78,"bold"), bg="blue", fg="white")
-Label.pack(pady=100)
+# Create the main window
+root = tk.Tk()
+root.title('Digital Clock')
 
-window.mainloop()
+# Create a label for the clock display
+clock_label = tk.Label(root, font=('Helvetica', 60))
+clock_label.pack()
+
+# Update the time every second
+update_time()
+
+# Start the event loop
+root.mainloop()
